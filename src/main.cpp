783220928b4fdc61bb625b498b606ebbf60e409b
Maloschnikow <123018032+Maloschnikow@users.noreply.github.com>
2023-08-25 09:08:03 +0200

    feat: std cfg path in wrk dir

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int main(int argc, char *argv[]) {
 
-  string configPath = "../rcs/config.txt";
+  string configPath = "config.txt";
   string imagePath;
   if(argc > 1) {
     imagePath = argv[1];
